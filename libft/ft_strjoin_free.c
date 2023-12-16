acc14f53bc5703c995d24f4c2bbdd63ad21b2438
Richard Luari <rluari@c1r2p10.42vienna.com>
2023-12-16 10:02:44 +0100

    Adding strjoin free

---
+++ libft/ft_strjoin_free.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin_free.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rluari <rluari@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/14 16:49:09 by rluari            #+#    #+#             */
+/*   Updated: 2023/12/14 16:49:43 by rluari           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin_free(char *s1, char *s2)
+{
+	char	*res;
+	int		full_len;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	full_len = ft_strlen(s1) + ft_strlen(s2);
+	res = (char *)malloc(full_len + 1);
+	if (!res)
+		return (NULL);
+	while (i < (int)ft_strlen(s1))
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	while (i < full_len)
+	{
+		res[i] = s2[j];
+		i++;
+		j++;
+	}
+	res[i] = '\0';
+	free(s1);
+	return (res);
+}
